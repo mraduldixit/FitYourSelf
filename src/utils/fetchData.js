4bbc8f41694d06b525956a02e118e8656d8a906c
Mradul Dixit <mraduldixit2002@gmail.com>
2022-07-08 12:22:51 +0530

    thirdone

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,5 @@
 export const exerciseOptions = {
-  // method: 'GET',
-  // headers: {
-  //   'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-  //   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-  // },
+ 
   method: 'GET',
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
@@ -14,6 +10,7 @@ export const exerciseOptions = {
 
 export const youtubeOptions = {
   method: 'GET',
+  url:'https://youtube-search-and-download.p.rapidapi.com',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
     'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
